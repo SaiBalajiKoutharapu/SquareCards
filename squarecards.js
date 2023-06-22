36e8bcb886b74f7a33c72fde5a49d59eaeeb4974
SaiBalajiKoutharapu <133504428+SaiBalajiKoutharapu@users.noreply.github.com>
2023-06-22 14:53:38 +0530

    Add files via upload

---
+++ squarecards.js
@@ -0,0 +1,29 @@
+document.getElementsByClassName("fa-brands fa-youtube")[0].
+addEventListener("click" , function(){
+    window.open("https://www.youtube.com/watch?v=MvgiPlVF0Tg&list=PLtMugc7g4GaqAVDZwQ_t1H6500ZGJzOgW&index=18");
+})
+
+document.getElementsByClassName("fa-brands fa-youtube")[1].
+addEventListener("click" , function(){
+    window.open("https://www.youtube.com/watch?v=MvgiPlVF0Tg&list=PLtMugc7g4GaqAVDZwQ_t1H6500ZGJzOgW&index=18");
+})
+
+document.getElementsByClassName("fa-brands fa-youtube")[2].
+addEventListener("click" , function(){
+    window.open("https://www.youtube.com/watch?v=MvgiPlVF0Tg&list=PLtMugc7g4GaqAVDZwQ_t1H6500ZGJzOgW&index=18");
+})
+
+document.getElementsByClassName("fa-brands fa-youtube")[3].
+addEventListener("click" , function(){
+    window.open("https://www.youtube.com/watch?v=MvgiPlVF0Tg&list=PLtMugc7g4GaqAVDZwQ_t1H6500ZGJzOgW&index=18");
+})
+
+document.getElementsByClassName("fa-brands fa-youtube")[4].
+addEventListener("click" , function(){
+    window.open("https://www.youtube.com/watch?v=MvgiPlVF0Tg&list=PLtMugc7g4GaqAVDZwQ_t1H6500ZGJzOgW&index=18");
+})
+
+document.getElementsByClassName("fa-brands fa-youtube")[5].
+addEventListener("click" , function(){
+    window.open("https://www.youtube.com/watch?v=MvgiPlVF0Tg&list=PLtMugc7g4GaqAVDZwQ_t1H6500ZGJzOgW&index=18");
+})
